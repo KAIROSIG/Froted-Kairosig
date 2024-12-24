773fdc2dee0e3544a9720a642ab769900f29da8c
Jean Pierre Moreira <doky88048@gmail.com>
2024-12-23 19:34:41 -0500

    ARREGLO APRA PRODUCCION API_BASE_URL

--- src/PanelAdmin.jsx
+++ src/PanelAdmin.jsx
@@ -4,14 +4,14 @@ import { useAuth } from './AuthContext';
 import axios from 'axios';
 import Swal from 'sweetalert2';
 
-const API_BASE_URL = 'http://localhost:5000/api';
+const API_BASE_URL = import.meta.env.VITE_API_URL;
 
 const api = axios.create({
   baseURL: API_BASE_URL,
   headers: {
     'Content-Type': 'application/json',
   },
-});
+}); 
 
 export default function PanelAdmin() {
   const { isAuthenticated, login } = useAuth();
